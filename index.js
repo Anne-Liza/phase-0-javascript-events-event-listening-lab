53cfcf1b2aad557572ecf23f5c1e55ab8bfec6f5
Anne <annewangui210@gmail.com>
2024-07-15 13:12:02 +0300

    phase-0-javascript-events-event-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+   const button = document. getElementById("button");
+    button.addEventListener ("click", function(e){
+    e.preventDefault();
+    console.log("Clicked");
+});
 }
+addingEventListener();
